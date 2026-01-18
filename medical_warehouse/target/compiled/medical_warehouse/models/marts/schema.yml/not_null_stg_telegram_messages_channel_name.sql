a7856be50217df778cb6483dd71e3c7919d05835
GrimVad3r <hnktesfaye@gmail.com>
2026-01-18 10:00:33 +0300

    Feat : Added Enhancements on the Dagester Pipeline

---
+++ medical_warehouse/target/compiled/medical_warehouse/models/marts/schema.yml/not_null_stg_telegram_messages_channel_name.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select channel_name
+from "medical_warehouse"."raw_staging"."stg_telegram_messages"
+where channel_name is null
+
+
---
+++ medical_warehouse/target/compiled/medical_warehouse/models/marts/schema.yml/not_null_stg_telegram_messages_views.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select views
+from "medical_warehouse"."raw_staging"."stg_telegram_messages"
+where views is null
+
+
